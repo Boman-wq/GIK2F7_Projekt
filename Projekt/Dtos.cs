d1370a757340a1966a8dd1bc3371c7d98833dfb4
Boman-wq <75782854+Boman-wq@users.noreply.github.com>
2022-05-20 10:16:56 +0200

    Update Dtos.cs

--- Projekt/Dtos.cs
+++ Projekt/Dtos.cs
@@ -5,5 +5,5 @@ namespace Catalog.Dtos
 {
     public record GameDto(Guid id, [Required] string Name, [Required] string Description, [Required][Range(1, 10)] int Grade, string Image);
     public record CreateGameDto([Required] string Name, [Required] string Description, [Required][Range(1,10)] int Grade, string Image);
-    public record UpdateGameDto([Required] string Name, [Required] string Description, [Required][Range(1, 10)] int Grade, string Image)
+    public record UpdateGameDto([Required] string Name, [Required] string Description, [Required][Range(1, 10)] int Grade, string Image);
 }
